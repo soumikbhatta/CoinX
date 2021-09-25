72d9cd9af0f2da1548ae5f2b76d9147224fde842
Soumikbhatta <soumikbhatta@gmail.com>
2021-09-26 04:43:57 +0530

    homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import millify from 'millify';
+import { Typography,Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            <Title level={2} className="heading">Global Crypto Coin Stats</Title>
+            <Row>
+                {/* antdesign has total 24 spaces */}
+                <Col span={12}>
+                    <Statistic title="Total CryptoCoins" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Exchanges" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Market Cap" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total 24h Volume" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Markets" value="5" />
+                </Col>
+            </Row>
+        
+        </>
     )
 }
 
