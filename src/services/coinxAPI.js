e29539586f98ca20aeb938d102b55292766b2c04
Soumikbhatta <soumikbhatta@gmail.com>
2021-09-26 08:29:02 +0530

    coinx api

---
+++ src/services/coinxAPI.js
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Provider } from 'react-redux';
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'f01062aeafmsh2a022aeb1906a6cp1ed77cjsn4eceab8936f9'
+//     }
+//   };
+
+const CoinApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'f01062aeafmsh2a022aeb1906a6cp1ed77cjsn4eceab8936f9'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createReq = (url) => ({ url, headers: CoinApiHeaders})
+
+export const coinxApi = createApi({
+    reducer: 'coinxApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCoins: builder.query({
+            query: () => createReq( '/coins')
+        })
+    })
+})
+
+export const {
+    useGetCoinsQuery,
+} = coinxApi;
+
+// make store.js
+// pass that store variable to a Provider
+// crete a api to fetch api data from rapidapi
+// connect api to store
