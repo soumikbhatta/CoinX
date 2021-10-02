fd1424c4f0bed29004555601e53dd9d64f2b4c71
Soumikbhatta <soumikbhatta@gmail.com>
2021-10-02 20:08:27 +0530

    env variables

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Menu, Typography, Avatar } from 'antd';
+import { Button, Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
--- src/services/coinxAPI.js
+++ src/services/coinxAPI.js
@@ -1,20 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': 'f01062aeafmsh2a022aeb1906a6cp1ed77cjsn4eceab8936f9'
-//     }
-//   };
+
 
 const CoinApiHeaders = {
-  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': 'f01062aeafmsh2a022aeb1906a6cp1ed77cjsn4eceab8936f9',
+  'x-rapidapi-host': 'process.env.API_HOST',
+  'x-rapidapi-key': 'process.env.API_KEY',
 };
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const baseUrl = 'process.env.API_URL';
 const createReq = (url) => ({ url, headers: CoinApiHeaders });
 
 export const coinxApi = createApi({
